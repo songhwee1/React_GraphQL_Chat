46cb0f11139af992b3448d9f85ffea9affa6a350
songhwee1 <songhwee1@naver.com>
2021-07-21 18:57:38 +0900

    add input, home

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import client from "./apolloClient";
+import Home from "./Home";
+import Input from "./input";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <Home />
+          <Input />
+        </ApolloHooksProvider>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
