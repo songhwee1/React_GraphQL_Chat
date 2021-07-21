e3da341dec1c440134a2b413e99059d5dfe21c6f
songhwee1 <songhwee1@naver.com>
2021-07-21 13:53:35 +0900

    add subscribe

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-
 import { GraphQLServer, PubSub } from "graphql-yoga";
+const pubsub = new PubSub();
+const NEW_CHAT = "NEW_CHAT";
 let chattingLog = [{
   id: 0,
   writer: "admin",
@@ -7,21 +8,22 @@ let chattingLog = [{
 }];
 
 const typeDefs = `
-type Chat {
-  id: Int!
-  writer: String!
-  description: String!
-}
-type Query {
-  chatting: [Chat]!
-}
-type Mutation {
-  write(writer: String!, description: String!): String!
-}
-type Subscription {
-  newChat: Chat
-}
+  type Chat {
+    id: Int!
+    writer: String!
+    description: String!
+  }
+  type Query {
+    chatting: [Chat]!
+  }
+  type Mutation {
+    write(writer: String!, description: String!): String!
+  }
+  type Subscription {
+    newChat: Chat
+  }
 `;
+
 const resolvers = {
   Query: {
     chatting: () => {
@@ -37,14 +39,24 @@ const resolvers = {
         description
       };
     chattingLog.push(newChat);
+    pubsub.publish(NEW_CHAT, {
+      newChat
+    })
     return "YES";
+    }
+  },
+  Subscription: {
+    newChat: {
+      subscribe: (_, __, { pubsub }) =>
+      pubsub.asyncIterator(NEW_CHAT)
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: typeDefs,
-  resolvers: resolvers
+  resolvers: resolvers,
+  context: { pubsub }
 });
 
 server.start(() => console.log("Graphql Server Running on host http://localhost:4000"));
